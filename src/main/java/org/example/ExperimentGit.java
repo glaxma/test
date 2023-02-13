66f40eb227155c94b7532d71837392e5194cb414
laxma <laxmareddy369@gmail.com>
2023-02-13 20:55:14 +0530

    first success project

---
+++ src/main/java/org/example/ExperimentGit.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class ExperimentGit {
+
+
+    public static void main(String[] args) {
+        // TODO document why this method is empty
+        test();
+    }
+    public static void test(){
+        System.out.println("the new name is new");
+        
+    }
+
+}
