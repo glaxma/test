d37ebd9c8f1e7e59c918337556e2d9e50988cb20
laxma <laxmareddy369@gmail.com>
2023-02-23 15:44:58 +0530

    removed 1

--- src/main/java/org/example/ExperimentGit.java
+++ src/main/java/org/example/ExperimentGit.java
@@ -24,19 +24,7 @@ public class ExperimentGit {
         System.out.println( map);
         System.out.println( map.containsValue("ravi"));
 
-
-        List<String> name=new ArrayList<>();
-        name.add("ramesh");
-        name.add("suresh");
-        name.add("ravesh");
-        name.add("pavesh");
-       String peoples=  String.format("the name one is %s",name);
-        System.out.println("total names are:"+peoples);
-         for(String people:name){
-             System.out.println(people.equalsIgnoreCase("pavesh"));
-         }
-         //test
-
     }
+    
 
 }
